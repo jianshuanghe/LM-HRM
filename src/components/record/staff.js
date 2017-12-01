3f4caae8381fb5f1e2a8e02e8228b567ea1faa59
jianshuanghe <274088960@qq.com>
2017-12-01 11:28:54 +0800

    jsh

--- src/components/record/staff.js
+++ src/components/record/staff.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+
+import Files from './management/files';
+
 
 class Staff extends React.Component{
 	render() {
 		return (
-			<h1>员工档案</h1>
+			<Files/>
 		)
 	}
 }
