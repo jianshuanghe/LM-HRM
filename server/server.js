154cf23364005d0c822c93dd6c8c63281df90047
aqlop <aqlop001@163.com>
2017-12-15 09:15:37 +0800

    接口代理

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/server0', require('./proxy0'));
 app.use('/server1', require('./proxy1'));
-app.use('/server2', require('./proxy2'));
 
 app.get('/', function (req, res) {
 	res.send('<h1>Hello World!</h1>');
