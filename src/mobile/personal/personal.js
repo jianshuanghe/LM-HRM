6a446b770565590964786e5830e0909c501b6000
aqlop <aqlop001@163.com>
2017-12-19 10:18:01 +0800

    添加移动端页面

---
+++ src/mobile/personal/personal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Personal extends React.Component{
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				移动端个人
+			</div>
+		)
+	}
+}
+
+export default Personal;
