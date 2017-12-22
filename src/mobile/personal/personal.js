3dc52152a7726835af8627189da283d992ac3f50
lixiaoyan <lixiaoyan_@gitlab.com>
2017-12-22 16:13:09 +0800

    个人中心

--- src/mobile/personal/personal.js
+++ src/mobile/personal/personal.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import portrait from './img/portrait.png'
+import './personal.css';
 class Personal extends React.Component{
 
 	constructor(props) {
@@ -9,7 +10,33 @@ class Personal extends React.Component{
 	render() {
 		return (
 			<div>
-				移动端个人
+				<div className="personal-head">
+					<img className="head-img" src={portrait} />
+					<p>JAVA工程师</p>
+					<p>工号：<b>LMBD-0001</b></p>
+				</div>
+				<div className="personal-con">
+					<ul>
+						<li>
+							<i></i>
+							<span>个人信息</span>
+							<b></b>
+						</li>
+						<li>
+							<i></i>
+							<span>岗位信息</span>
+							<b></b>
+						</li>
+						<li>
+							<i></i>
+							<span>报销记录</span>
+							<b></b>
+						</li>
+					</ul>
+				</div>
+				<div className="personal-foot">
+					<span>退出登录</span>
+				</div>
 			</div>
 		)
 	}
